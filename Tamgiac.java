5b9a0c1f11317acb84fbd8ed8e4e1a728d32ed8b
thaonguyen296 <44283110+thaonguyen296@users.noreply.github.com>
2019-03-14 22:21:04 +0700

    Add files via upload

---
+++ Tamgiac.java
@@ -0,0 +1,43 @@
+package session3_lab;
+import java.util.Scanner;
+public class Tamgiac {
+    private double a=3;
+    private double b=4;
+    private double c=5;
+
+
+    public Tamgiac(double a, double b, double c) {
+        this.a =a;
+        this.b =b;
+        this.c =c;
+        this.checkTamgiac();
+    }
+
+
+    public void checkTamgiac(){
+        while (this.a+this.b<=this.c || this.a+this.c<=this.b || this.c+this.b<=this.a) {
+            Scanner info = new Scanner(System.in);
+            System.out.println("Nhap 3 canh:");
+            this.a = info.nextDouble();
+            this.b = info.nextDouble();
+            this.c = info.nextDouble();
+        }
+    }
+
+    public double chuvi(){
+        return this.a+this.b+this.c;
+    }
+
+    public double dientich(){
+        double p = (this.a+this.b+this.c)/2.0;
+        double a = p*(p-this.a)*(p-this.b)*(p-this.c);
+        return java.lang.Math.sqrt(a);
+    }
+
+    public static void main(String[] args){
+        //Tao tam giac moi, do dai 3 canh 1,2,3
+        Tamgiac tg = new Tamgiac(1,5,7);
+        System.out.println(tg.dientich());
+
+    }
+}
